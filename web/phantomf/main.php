248e991ef6ab03aa14d686fd396cf0a7a89c8bb5
Curtis Wakley <curtis.wakley@gmail.com>
2018-02-08 19:47:20 -0700

    Added sign in availability

--- web/phantomf/main.php
+++ web/phantomf/main.php
@@ -2,28 +2,6 @@
 
 	include 'title.php';
 
-	$dbUrl = getenv('DATABASE_URL');
-
-	if (empty($dbUrl))
-	{
-		$dbUrl = "postgres://rahcccsbnamvss:e32675dcc727946b100dd92eaec7efee2edd946b23887d34a0502e1e6adca480@ec2-54-83-204-230.compute-1.amazonaws.com:5432/d8jro2qh5v71i4";
-	}
-
-	$dbopts = parse_url($dbUrl);
-
-	$dbHost = $dbopts["host"];
-	$dbPort = $dbopts["port"];
-	$dbUser = $dbopts["user"];
-	$dbPassword = $dbopts["pass"];
-	$dbName = ltrim($dbopts["path"],'/');
-
-	try {
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-	}
-	catch (PDOException $ex) {
-		echo 'Error!: ' . $ex->getMessage();
-		die();
-	}
 ?>
 
 <html>
@@ -77,7 +55,7 @@
       					<option value="Portal 2">Portal 2</option>
       				</select>
       				<input type="submit" name="search" value="Search">
-      			</form>
+      			</form><br>
 
       			<?php
 
--- web/phantomf/title.php
+++ web/phantomf/title.php
@@ -2,6 +2,48 @@
 
 	session_start();
 
+	$dbUrl = getenv('DATABASE_URL');
+
+	if (empty($dbUrl))
+	{
+		$dbUrl = "postgres://rahcccsbnamvss:e32675dcc727946b100dd92eaec7efee2edd946b23887d34a0502e1e6adca480@ec2-54-83-204-230.compute-1.amazonaws.com:5432/d8jro2qh5v71i4";
+	}
+
+	$dbopts = parse_url($dbUrl);
+
+	$dbHost = $dbopts["host"];
+	$dbPort = $dbopts["port"];
+	$dbUser = $dbopts["user"];
+	$dbPassword = $dbopts["pass"];
+	$dbName = ltrim($dbopts["path"],'/');
+
+	try {
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	}
+	catch (PDOException $ex) {
+		echo 'Error!: ' . $ex->getMessage();
+		die();
+	}
+
+	if (isset($_POST['loginSubmit']))
+	{
+		if($_POST['usrname'] != "")
+		{
+			foreach($db->query('SELECT username, password FROM users') as $log)
+			{
+				if($log['username'] == $_POST['usrname'] && $log['password'] == $_POST['pass'])
+				{
+					$_SESSION['usr'] = $log['username'];
+				}
+			}
+		}
+	}
+
+	if (isset($_POST['logout']))
+	{
+		session_unset();
+	}
+
 ?>
 <head>
 	<title>Phantom Foxtrot Server Hosting</title>
@@ -17,11 +59,24 @@
 	<a href="request.php">Submit Request</a>
 	<a href="chat.php">Chat</a>
 	<a href="join.php">Join us!</a>
-	<form action="" method="post" name="login" class="login">
+	<?php
+		if (!isset($_SESSION['usr']))
+		{
+	?>
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" name="login" class="login">
 		Sign In:
-		<input type="text" name="usrname" size="10" placeholder="Username"> 
-		<input type="password" name="pass" size="10" placeholder="Password"> 
+		<input type="text" name="usrname" size="10" placeholder="Username"> 	<input type="password" name="pass" size="10" placeholder="Password"> 
 		<input type="submit" name="loginSubmit" value="Login">
 	</form>
+	<?php
+		}
+		else {
+			echo 'Welcome, ' . $_SESSION['usr'];
+	?>
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" name="logout">
+			<input type="submit" name="logout" value="Logout">
+	<?php
+		}
+	?>
 </div>
 </center>
